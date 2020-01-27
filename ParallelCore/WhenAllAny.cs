b3edd3ac9071205e44b53beb041c15aa42e313ba
Serge Gagnon <serge.gagnon.1@live.ca>
2020-01-27 12:42:36 -0500

    Début de WhenAll, pas terminé

---
+++ ParallelCore/WhenAllAny.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelCore
+{
+    public class WhenAllAny
+    {
+        readonly Random randSource = new Random();
+
+        public WhenAllAny()
+        {
+        }
+
+
+        private async Task<int> CreateRandomOne()
+        {
+            int result = 0;
+            int accu = 0;
+            int range = randSource.Next();
+
+            await Task.Run(() =>
+            {
+                while (accu < range )
+                {
+                    result = accu;
+                    accu++;
+                }
+            }).ConfigureAwait(true);
+            Console.WriteLine("CreateRandomOne finish with number {0}", result);
+            return result;
+        }
+
+        private async Task<int> CreateRandomTwo()
+        {
+            int result = 0;
+            int accu = 0;
+            int range = randSource.Next();
+
+            await Task.Run(() =>
+            {
+                while (accu < range)
+                {
+                    result = accu;
+                    accu++;
+                }
+            }).ConfigureAwait(true);
+            Console.WriteLine("CreateRandomTwo finish with number {0}", result);
+            return result;
+        }
+    }
+}
